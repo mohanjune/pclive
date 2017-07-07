70626e81758706a0840b0e7317c8cebad074e23e
Mohan Muddana <mohanjune@gmail.com>
2017-07-07 12:13:21 +0530

    Initial Commit

---
+++ src/main/java/com/pclive/demo/com/pclive/demo/CommandController.java
@@ -0,0 +1,74 @@
+package com.pclive.demo.com.pclive.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CommandController {
+	
+	 /** Root Command */	
+	 @RequestMapping("/")
+	 public String index() {
+		 return "index";
+	 }
+	 
+
+	 /** Header Commands */
+	 
+	 @RequestMapping("/national")
+	 public String national() {
+		 return "national";
+	 }
+	 
+	 @RequestMapping("/international")
+	 public String international() {
+		 return "international";
+	 }
+
+	 @RequestMapping("/newsvideos")
+	 public String videos() {
+		 return "newsvideos";
+	 }
+	 
+	 @RequestMapping("/blogs")
+	 public String blogs() {
+		 return "blogs";
+	 }
+	 
+	 /** Footer Commands */
+	 
+	 @RequestMapping("/contact")
+	 public String contacts() {
+		 return "contact";
+	 }
+	 
+	 @RequestMapping("/aboutus")
+	 public String aboutus() {
+		 return "aboutus";
+	 }
+	 
+	 @RequestMapping("/disclaimer")
+	 public String disclaimer() {
+		 return "disclaimer";
+	 }
+	 
+	 @RequestMapping("/advertise")
+	 public String advertise() {
+		 return "advertise";
+	 }
+
+	 @RequestMapping("/serviceterms")
+	 public String serviceterms() {
+		 return "serviceterms";
+	 }
+	 
+	 @RequestMapping("/feedback")
+	 public String feedback() {
+		 return "feedback";
+	 }
+	 
+	 @RequestMapping("/editorial")
+	 public String editorial() {
+		 return "editorial";
+	 }
+}
